442fb12e739de7111abe4d22be761cab784185f1
bohjak <gh@bohacek.dev>
2022-11-14 11:38:38 +0100

    fix diagonals check logic

---
+++ src/check.ts
@@ -0,0 +1,47 @@
+// FIXME: this approach doesn't scale well with larger fields — especially if the winning slice is smaller than field dimensions — and the results aren't easily usable by a player AI
+
+import { CellState, GameStage } from "./field-slice";
+
+const isX = (cell: CellState): boolean => cell === CellState.X;
+const isO = (cell: CellState): boolean => cell === CellState.O;
+
+export const checkRows = (cells: CellState[]): boolean => {
+  const rows: CellState[][] = [];
+  for (let i = 1; i <= 3; ++i) {
+    rows.push(cells.slice((i - 1) * 3, i * 3));
+  }
+  if (rows.some((row) => row.every(isX) || row.every(isO))) return true;
+  return false;
+};
+
+export const checkColumns = (cells: CellState[]): boolean => {
+  const cols: CellState[][] = [[], [], []];
+  for (let i = 0; i < cells.length; ++i) {
+    cols[i % 3].push(cells[i]);
+  }
+  if (cols.some((col) => col.every(isX) || col.every(isO))) return true;
+  return false;
+};
+
+export const checkDiags = (cells: CellState[]): boolean => {
+  const diags: CellState[][] = [[], []];
+  for (let i = 0; i < cells.length; ++i) {
+    if (i % 4 === 0) diags[0].push(cells[i]);
+    if (i % 2 === 0 && i % 8 !== 0) diags[1].push(cells[i]);
+  }
+  if (diags.some((diag) => diag.every(isX) || diag.every(isO))) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const checkEndCondition = (
+  cells: CellState[]
+): GameStage.Tie | GameStage.Win | null => {
+  if (checkRows(cells) || checkColumns(cells) || checkDiags(cells)) {
+    return GameStage.Win;
+  }
+  if (cells.every((cell) => cell !== CellState.Empty)) return GameStage.Tie;
+  return null;
+};
--- src/field.tsx
+++ src/field.tsx
@@ -1,47 +1,7 @@
 import React from "react";
+import { checkEndCondition } from "./check";
 import { CellState, fieldAction, FieldState, GameStage } from "./field-slice";
 import { AppDispatch, RootState, useDispatch, useSelector } from "./store";
-
-// FIXME: this approach doesn't scale well with larger fields — especially if the winning slice is smaller than field dimensions — and the results aren't easily usable by a player AI
-
-const isX = (cell: CellState): boolean => cell === CellState.X;
-const isO = (cell: CellState): boolean => cell === CellState.O;
-
-const checkRows = (cells: FieldState["cells"]): boolean => {
-  const rows: CellState[][] = [];
-  for (let i = 1; i <= 3; ++i) {
-    rows.push(cells.slice((i - 1) * 3, i * 3));
-  }
-  if (rows.some((row) => row.every(isX) || row.every(isO))) return true;
-  return false;
-};
-const checkColumns = (cells: FieldState["cells"]): boolean => {
-  const cols: CellState[][] = [[], [], []];
-  for (let i = 0; i < cells.length; ++i) {
-    cols[i % 3].push(cells[i]);
-  }
-  if (cols.some((col) => col.every(isX) || col.every(isO))) return true;
-  return false;
-};
-const checkDiags = (cells: FieldState["cells"]): boolean => {
-  const diags: CellState[][] = [[], []];
-  for (let i = 0; i < cells.length; ++i) {
-    if (i % 4 === 0) diags[0].push(cells[i]);
-    if ((i + 2) % 4 === 0 && i % 8 !== 0) diags[1].push(cells[i]);
-  }
-  if (diags.some((diag) => diag.every(isX) || diag.every(isO))) return true;
-  return false;
-};
-
-const checkEndCondition = (
-  cells: FieldState["cells"]
-): GameStage.Tie | GameStage.Win | null => {
-  if (checkRows(cells) || checkColumns(cells) || checkDiags(cells)) {
-    return GameStage.Win;
-  }
-  if (cells.every((cell) => cell !== CellState.Empty)) return GameStage.Tie;
-  return null;
-};
 
 const makeMove =
   (idx: number) => (dispatch: AppDispatch, getState: () => RootState) => {
